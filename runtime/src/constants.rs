c6a6f52d8f1b12f56bd20741a9f3d25d7238314f
Mohak-Singhal <mohaksinghal516@gmail.com>
2026-07-18 02:34:07 +0530

    update port

--- runtime/src/constants.rs
+++ runtime/src/constants.rs
@@ -5,5 +5,5 @@ pub const HEARTBEAT_INTERVAL_SECS: u64 = 5;
 pub const NODE_TIMEOUT_SECS: u64 = 15;
 
 pub const DEFAULT_HTTP_PORT: u16 = 8080;
-pub const MULTICAST_GROUP: &str = "239.255.0.1";
+pub const MULTICAST_GROUP: &str = "239.255.100.100";
 pub const MULTICAST_PORT: u16 = 53317;
